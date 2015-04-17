69d85e318179e137fc1c66576fc932a0e3265053
Ryan Langman <rlangman@caltech.edu>
2015-04-17 11:30:22 -0700

    Removed random whitespace and made test names more detailed.

--- config_parser_test.cc
+++ config_parser_test.cc
@@ -4,49 +4,34 @@
 TEST(NginxConfigParserTest, SimpleConfig) {
   NginxConfigParser parser;
   NginxConfig out_config;
-
   bool success = parser.Parse("test_configs/example_config", &out_config);
-
   EXPECT_TRUE(success);
-
 }
 
-TEST(NginxConfigParserTest, SimpleServerConfig) {
+TEST(NginxConfigParserTest, SimpleServerTest) {
   NginxConfigParser parser;
   NginxConfig out_config;
-
   bool success = parser.Parse("test_configs/simple_server_config", &out_config);
-
   EXPECT_TRUE(success);
-
 }
 
-TEST(NginxConfigParserTest, LocationConfig) {
+TEST(NginxConfigParserTest, ServerWithLocationTest) {
   NginxConfigParser parser;
   NginxConfig out_config;
-
   bool success = parser.Parse("test_configs/location_config", &out_config);
-
   EXPECT_TRUE(success);
-
 }
 
-TEST(NginxConfigParserTest, HttpConfig) {
+TEST(NginxConfigParserTest, HttpWithServerTest) {
   NginxConfigParser parser;
   NginxConfig out_config;
-
   bool success = parser.Parse("test_configs/http_config", &out_config);
-
   EXPECT_TRUE(success);
-
 }
 
 TEST(NginxConfigParserTest, ComplicatedConfig) {
   NginxConfigParser parser;
   NginxConfig out_config;
-
   bool success = parser.Parse("test_configs/complicated_config", &out_config);
-
   EXPECT_TRUE(success);
-
 }

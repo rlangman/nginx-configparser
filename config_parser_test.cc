e9184335253557c31feff1bf1bb2167ab2bf9584
Ryan Langman <rlangman@caltech.edu>
2015-04-16 17:56:28 -0700

    Added unit tests for nginx-config parser.

--- config_parser_test.cc
+++ config_parser_test.cc
@@ -5,7 +5,48 @@ TEST(NginxConfigParserTest, SimpleConfig) {
   NginxConfigParser parser;
   NginxConfig out_config;
 
-  bool success = parser.Parse("example_config", &out_config);
+  bool success = parser.Parse("test_configs/example_config", &out_config);
 
   EXPECT_TRUE(success);
+
+}
+
+TEST(NginxConfigParserTest, SimpleServerConfig) {
+  NginxConfigParser parser;
+  NginxConfig out_config;
+
+  bool success = parser.Parse("test_configs/simple_server_config", &out_config);
+
+  EXPECT_TRUE(success);
+
+}
+
+TEST(NginxConfigParserTest, LocationConfig) {
+  NginxConfigParser parser;
+  NginxConfig out_config;
+
+  bool success = parser.Parse("test_configs/location_config", &out_config);
+
+  EXPECT_TRUE(success);
+
+}
+
+TEST(NginxConfigParserTest, HttpConfig) {
+  NginxConfigParser parser;
+  NginxConfig out_config;
+
+  bool success = parser.Parse("test_configs/http_config", &out_config);
+
+  EXPECT_TRUE(success);
+
+}
+
+TEST(NginxConfigParserTest, ComplicatedConfig) {
+  NginxConfigParser parser;
+  NginxConfig out_config;
+
+  bool success = parser.Parse("test_configs/complicated_config", &out_config);
+
+  EXPECT_TRUE(success);
+
 }
